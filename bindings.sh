75779cdb43243bc163e068f349baa5f90d264803
sanjeeb <code.sanjeeb@gmail.com>
2024-12-11 01:22:14 +0530

    Update bindings.sh

--- bindings.sh
+++ bindings.sh
@@ -2,15 +2,16 @@
 
 bindings=""
 
-while IFS= read -r line || [ -n "$line" ]; do
-  if [[ ! "$line" =~ ^# ]] && [[ -n "$line" ]]; then
-    name=$(echo "$line" | cut -d '=' -f 1)
-    value=$(echo "$line" | cut -d '=' -f 2-)
-    value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
+# Iterate over all environment variables
+while IFS='=' read -r name value; do
+  # Exclude any variables that you don't want, such as Render's internal variables
+  if [[ ! "$name" =~ ^RENDER_|^PATH$|^HOME$|^PWD$|^SHLVL$|^_ ]] && [[ -n "$value" ]]; then
     bindings+="--binding ${name}=${value} "
   fi
 done < <(env)
 
+# Trim trailing spaces
 bindings=$(echo $bindings | sed 's/[[:space:]]*$//')
 
+# Print the bindings
 echo $bindings
