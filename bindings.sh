a7feb2b719afda41c2edee60366ffa4e631ae84a
sanjeeb <code.sanjeeb@gmail.com>
2024-12-11 00:59:09 +0530

    Update bindings.sh

--- bindings.sh
+++ bindings.sh
@@ -2,20 +2,15 @@
 
 bindings=""
 
-# Iterate over all environment variables
-while IFS='=' read -r name value; do
-  # Skip Render's system variables or variables starting with underscores if needed
-  if [[ "$name" != "RENDER*" ]] && [[ "$name" != "_"* ]]; then
-    # Escape special characters in value
-    value=$(printf '%q' "$value")
-
-    # Append to bindings
+while IFS= read -r line || [ -n "$line" ]; do
+  if [[ ! "$line" =~ ^# ]] && [[ -n "$line" ]]; then
+    name=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2-)
+    value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
     bindings+="--binding ${name}=${value} "
   fi
 done < <(env)
 
-# Remove trailing spaces from bindings
-bindings=$(echo "$bindings" | sed 's/[[:space:]]*$//')
+bindings=$(echo $bindings | sed 's/[[:space:]]*$//')
 
-# Output the final bindings string
-echo "$bindings"
+echo $bindings
